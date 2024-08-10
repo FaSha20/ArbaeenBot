1ee9c860b3d293f2a78e3f402a505783e0424156
fatemeh <f.shahhosseini.20@gmail.com>
2024-08-10 16:03:52 +0330

    some changes made.

--- BaleBot/ArbaeenBot.py
+++ BaleBot/ArbaeenBot.py
@@ -1,10 +1,16 @@
 from bale import *
-bot = Bot(token="152238739:EU8ukOoTPGfSknbp2zqZfzAFu5I8l8ah5Rn7NznW")
-    
+from check_id_code import *
+from decouple import config
+bot = Bot(token=config('MAIN_TOKEN'))
+
+
+database = dict()
+
 
 @bot.event
 async def on_ready():
 	print(bot.user.username, "is Ready!")
+	
 
 @bot.event
 async def on_update(update: Update):
@@ -15,64 +21,128 @@ async def on_update(update: Update):
 
 @bot.event
 async def on_message(message: Message):
-	if message.content == '/start': # to get caption or text of message
+
+	if message.content == '/start': 
+
+		database = dict()
+		database['id'] = message.author.first_name
 		await message.reply(
-			f"*سلام {message.author.first_name}, به بات همیار اربعین خوش اومدی :)*",
-			components=MenuKeyboardMarkup().add(MenuKeyboardButton('مداحی و  محتوای صوتی')).add(MenuKeyboardButton('گمشدگان و پیداشدگان')).add(MenuKeyboardButton('تجربیات اربعین'))
+			f"سلام {message.author.first_name}, به دستیار بروزرسانی اطلاعات موکب‌های اربعین خوش آمدید 👋\nلطفا *کد شناسایی موکب* خود را وارد کنید:",
 		)
+		def answer_checker(m: Message):
+			return message.author == m.author and bool(message.text)
+		answer_obj: Message = await bot.wait_for('message', check=answer_checker)
+
+		if check_id_code(answer_obj.text):
+			database['mukeb_name'] = ""
+			reply_markup = InlineKeyboardMarkup()
+			reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="food_0"), row=1)
+			reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="food_10"), row=2)
+			reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="food_100"), row=3)
+			return await answer_obj.reply(
+				f' موکب شما شناسایی شد.\n 🍲 لطفا ظرفیت *پذیرایی* موکب برای امروز را اعلام بفرمایید',
+				components=reply_markup
+			)
+		else:
+			return await answer_obj.reply(
+				f' موکب با این شماره *شناسایی نشد*. لطفا از صحت کد ورودی اطمینان یافته و دوباره امتحان کنید',
+				components=MenuKeyboardMarkup().add(MenuKeyboardButton('/start'))
+			)		
 	
-	elif  message.photos:
-		print(message)
-		file = open('./savedImages/attachment.png', 'wb')
-		await message.document.save_to_memory(file)
-		return await message.reply("تصویر شما ذخیره شد")
 	
-	elif message.content == 'مداحی و  محتوای صوتی':
-		reply_markup = InlineKeyboardMarkup()
-		reply_markup.add(InlineKeyboardButton(text="محمود کریمی", callback_data="karimi"), row=1)
-		reply_markup.add(InlineKeyboardButton(text="مهدی رسولی", callback_data="rasouli"), row=1)
-		reply_markup.add(InlineKeyboardButton(text="مجید بنی فاطمه", callback_data="banifatemeh"), row=2)
-		reply_markup.add(InlineKeyboardButton(text="سعید حدادیان", callback_data="haddadian"), row=2)
-		await message.reply(
-			f"* کدام مداحی را ترجیح میدهی؟*",
-			components=reply_markup
-		)
-		
-	elif  message.audio:
-		print(message)
-		file = open('./savedAudios/attachment.mp3', 'wb')
-		await message.document.save_to_memory(file)
-		file = open('./savedAudios/attachment.mp3', 'rb').read()
-		return await message.reply_audio(audio=InputFile(file), caption="صوت شما در سیستم ذخیره شد :)")
-	
-	else:
-		await message.reply(
-			f"لطفا دستور معتبر وارد کنید :)",
-			components=MenuKeyboardMarkup().add(MenuKeyboardButton('مداحی و  محتوای صوتی')).add(MenuKeyboardButton('گمشدگان و پیداشدگان')).add(MenuKeyboardButton('تجربیات اربعین'))
-		)
+	# else:
+	# 	reply_markup = InlineKeyboardMarkup()
+	# 	reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="food_0"), row=1)
+	# 	reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="food_10"), row=2)
+	# 	reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="food_100"), row=3)
+	# 	await message.reply(
+	# 		f"*سلام {message.author.first_name}, به بات همیار اربعین خوش اومدی :)*",
+			
+	# 	)
 		
 
 @bot.event
 async def on_callback(callback: CallbackQuery):
 	print("on call back")
-	if callback.data == "karimi":
+
+	if callback.data == "food_0":
+		database["food_cap"] = 0
 		reply_markup = InlineKeyboardMarkup()
-		reply_markup.add(InlineKeyboardButton(text="محمود کریمی", callback_data="karimi"), row=1)
-		reply_markup.add(InlineKeyboardButton(text="مهدی رسولی", callback_data="rasouli"), row=1)
-		reply_markup.add(InlineKeyboardButton(text="مجید بنی فاطمه", callback_data="banifatemeh"), row=2)
-		reply_markup.add(InlineKeyboardButton(text="سعید حدادیان", callback_data="haddadian"), row=2)
+		reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="rest_0"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="rest_10"), row=2)
+		reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="rest_100"), row=3)
 		await callback.message.reply(
-			f"* مداحی‌ها*",
+			f"💤 لطفا ظرفیت *استراحتگاهی* موکب برای امروز را اعلام بفرمایید",
 			components=reply_markup
 		)
-		file = open('madahi\\Karimi\\saghi_teflan_hosein.mp3', 'rb').read()
-		audio = InputFile(file)
-		await callback.message.reply_audio(audio=audio, caption="این مداحی شماست :)")
-	elif callback.data == "rasouli":
-		file = open('madahi\\Helali\\آقام آقام آقام آقام حسین.mp3', 'rb').read()
-		audio = InputFile(file)
-		await callback.message.reply_audio(audio=audio, caption="این مداحی شماست :)")
+	
+	elif callback.data == "food_10":
+		database["food_cap"] = 10
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="rest_0"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="rest_10"), row=2)
+		reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="rest_100"), row=3)
+		await callback.message.reply(
+			f"💤 لطفا ظرفیت *استراحتگاهی* موکب برای امروز را اعلام بفرمایید",
+			components=reply_markup
+		)
+
+	elif callback.data == "food_100":
+		database["food_cap"] = 100
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="rest_0"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="rest_10"), row=2)
+		reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="rest_100"), row=3)
+		await callback.message.reply(
+			f"💤 لطفا ظرفیت *استراحتگاهی* موکب برای امروز را اعلام بفرمایید",
+			components=reply_markup
+		)
 		
+	elif callback.data == "rest_0":
+		database["rest_cap"] = 0
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="ثبت", callback_data="register"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ویرایش", callback_data="edit"), row=2)
+		await callback.message.reply(
+			f'*اطلاعات دریافت شده*\n نام موکب : {""}\n ظرفیت پذیرایی: {database["food_cap"]}\n ظرفیت استراحتگاهی: {database["rest_cap"]}\n ',
+			components=reply_markup
+		)
+	
+	elif callback.data == "rest_10":
+		database["rest_cap"] = 10
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="ثبت", callback_data="register"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ویرایش", callback_data="edit"), row=2)
+		await callback.message.reply(
+			f'*اطلاعات دریافت شده*\n نام موکب : {""}\n ظرفیت پذیرایی: {database["food_cap"]}\n ظرفیت استراحتگاهی: {database["rest_cap"]}\n ',
+			components=reply_markup
+		)
+
+	elif callback.data == "rest_100":
+		database["rest_cap"] = 100
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="ثبت", callback_data="register"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ویرایش", callback_data="edit"), row=2)
+		await callback.message.reply(
+			f'*اطلاعات دریافت شده*\n نام موکب : {""}\n ظرفیت پذیرایی: {database["food_cap"]}\n ظرفیت استراحتگاهی: {database["rest_cap"]}\n ',
+			components=reply_markup
+		)
+	
+	elif callback.data == "edit":
+		reply_markup = InlineKeyboardMarkup()
+		reply_markup.add(InlineKeyboardButton(text="صفر", callback_data="food_0"), row=1)
+		reply_markup.add(InlineKeyboardButton(text="ده تا صد نفر", callback_data="food_10"), row=2)
+		reply_markup.add(InlineKeyboardButton(text="بیشتر از صد نفر", callback_data="food_100"), row=3)
+		return await callback.message.reply(
+			f' 🍲 لطفا *ظرفیت پذیرایی* موکب برای امروز را اعلام بفرمایید',
+			components=reply_markup
+		)
+	
+	elif callback.data == "register":
+		update_record(database)
+		await callback.message.reply(
+			f"*اطلاعات با موفقیت ثبت شد. متشکریم از زمانی که اختصاص دادید 🙏 *"
+		)
 
 
 bot.run()
---
+++ BaleBot/check_id_code.py
@@ -0,0 +1,11 @@
+# VO6byj56flizYSX9
+# fshahhosseini20
+# mongodb+srv://cluster0.0uoaj.mongodb.net/" --apiVersion 1 --username fshahhosseini20
+
+
+def check_id_code(id):
+    return True
+
+
+def update_record(databese):
+    return
